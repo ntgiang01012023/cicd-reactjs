494097c768c23899b809c6c7fd007e305b59b3af
ntgiang01012023 <ntgiang01012023@email.com>
2024-12-14 09:36:39 +0700

    Fix deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/cicd-reactjs/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
